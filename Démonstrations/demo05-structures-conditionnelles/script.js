00bd7df554f05e2c95958e2ab690e02246136990
Quentin Geerts <quentin.geerts@bstorm.be>
2026-01-29 14:50:58 +0100

    Démonstration 05 - Structures conditionnelles

---
+++ Démonstrations/demo05-structures-conditionnelles/script.js
@@ -0,0 +1,39 @@
+/*
+    Démonstration 05 - Structures conditionnelles
+*/
+
+// 1.  Structure "if"
+
+/* -- Syntaxe
+
+    if (condition) {
+        // bloc d'instructions
+    }
+    else if (condition2) {
+        // bloc d'instructions
+    }
+    else {
+        // bloc d'instructions par défaut
+    }
+
+*/
+
+// 2.  Structure "switch"
+
+/* -- Syntaxe
+
+    switch (expression) {
+        case value1:
+        case value2:
+            // bloc d'instructions
+            break;
+        case value3:
+            // bloc d'instructions
+            break;
+
+        default:
+            // bloc d'instructions
+            break;
+    }
+
+*/
